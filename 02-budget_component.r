e43337b176a585c76aed7ea72503a7df2f597d18
Thomas Levine <occurrence@thomaslevine.com>
2012-12-04 11:46:03 -0500

    adjusted appropriation

--- 02-budget_component.r
+++ 02-budget_component.r
@@ -21,29 +21,24 @@ tax <- read.csv('budget/appendix_a1-tax.csv')
 
 # Join
 tax <- join(tax, inflation)
-tax$tax.rate.adj.2009 <- tax$tax.rate/tax$CPIAUCSL_2009
+tax$tax.rate.adj.2009 <- tax$tax.rate/tax$CPIAUCSL.2009
 budget <- join(tax, funds)
 
 # Use the adopted budget if available; otherwise, use the tentative budget.
 budget <- ddply(budget, c('year', 'fund'), function(df){df[order(df$budget),][1,]})
 
 
-budget.1 <- sqldf('select year, fund, CPIAUCSL_2012, appropriation from budget join inflation on budget.year = inflation.year')
-budget.1$appropriation.2012 <- budget.1$appropriation / budget.1$CPIAUCSL_2012
+budget$appropriation.2012 <- budget$appropriation / budget$CPIAUCSL.2012
 
 .melt.vars <- c(
    "year",
-   "budget",
-   "assessed",
-   "tax.rate",
-   "CPIAUCSL_2012",
-   "CPIAUCSL_2009",
-   "tax.rate.adj.2009",
    "fund",
-#  "appropriation",
-   "Non.Prop.Tax.Revenue",
-   "Approp.Fund.Balance",
-   "Amount.To.Be.Raised.By.Taxes"
+   "tax.rate",
+   "CPIAUCSL.2012",
+   "tax.rate.adj.2009"
 )
-appropriations <- ggplot(melt(budget, .melt.vars)) +
-  aes(x=year, y = value, lty = variable, color = fund, group = paste(variable, fund), color = fund) + geom_line()
+.melted <- melt(budget[c(.melt.vars, 'appropriation', 'appropriation.2012')], .melt.vars)
+appropriations <- ggplot(.melted) +
+  aes(x = year, y = value, group = paste(variable, fund)) +
+  aes(lty = variable, color = fund) +
+  geom_line()
