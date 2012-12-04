558a011eaee58ee6f249fc1f1d19f840969f8747
Thomas Levine <occurrence@thomaslevine.com>
2012-12-04 00:57:09 -0500

    2009

--- explore-budget.r
+++ explore-budget.r
@@ -7,10 +7,11 @@ library(quantmod)
 
 # Inflation
 getSymbols("CPIAUCSL", src='FRED')
-inflation.xts <- CPIAUCSL[paste(2006:2012, '-05-01', sep = '')] / CPIAUCSL['2012-05-01'][[1]]
+inflation.xts <- CPIAUCSL[paste(2006:2012, '-05-01', sep = '')]
 inflation <- data.frame(
   year = paste(2006:2012, 2007:2013, sep = '-'),
-  inflation.xts[,1]
+  CPIAUCSL.2012 = as.vector(inflation.xts[,1] / CPIAUCSL['2012-05-01'][[1]]),
+  CPIAUCSL.2009 = as.vector(inflation.xts[,1] / CPIAUCSL['2009-05-01'][[1]])
 )
 
 # Load budget data
@@ -22,7 +23,7 @@ tax <- ddply(tax, 'year', function(df){df[order(df$budget),][1,]})
 
 # Join
 tax <- join(tax, inflation)
-tax$tax.rate.adj <- tax$tax.rate/tax$CPIAUCSL
+tax$tax.rate.adj.2009 <- tax$tax.rate/tax$CPIAUCSL.2009
 budget <- join(tax, funds)
 
 # Tax rate over time
@@ -32,14 +33,14 @@ tax.by.year <- ggplot(tax) + aes(x = year, y = tax.rate) + geom_point() +
   scale_y_continuous('Village tax rate (percent of assessed value)', labels = percent)
 
 # Tax rate by value of a dollar in 2012 dollars
-tax.by.inflation <- ggplot(tax) + aes(x = CPIAUCSL, y = tax.rate, label = year) + geom_text() +
+tax.by.inflation <- ggplot(tax) + aes(x = CPIAUCSL.2012, y = tax.rate, label = year) + geom_text() +
   labs(title = 'Village tax rate and inflation increase somewhat proportionately.') +
   scale_x_continuous('Value of a dollar in May 2012 dollars', labels = dollar) +
   scale_y_continuous('Village tax rate (percent of assessed value)', labels = percent)
 
 # Inflation-adjusted tax rate by value of a dollar
-adj.tax.by.inflation <- ggplot(melt(tax, c('year', 'budget', 'CPIAUCSL', 'assessed'))) +
-  aes(x = CPIAUCSL, y = value, group = variable, label = year, linetype = variable) +
+adj.tax.by.inflation <- ggplot(melt(tax, c('year', 'budget', 'CPIAUCSL.2009', 'CPIAUCSL.2012', 'assessed'))) +
+  aes(x = CPIAUCSL.2009, y = value, group = variable, label = year, linetype = variable) +
   labs(title = '') +
   scale_x_continuous('Value of a dollar in May 2012 dollars', labels = dollar) +
   scale_y_continuous('Inflation-adjusted village tax rate (percent of assessed value)', labels = percent) +
