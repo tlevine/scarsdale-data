175c742960b4764fc72a4c52fbd6cd2373bf15e0
Thomas Levine <occurrence@thomaslevine.com>
2012-12-03 23:56:28 -0500

    first plot

--- budget-2008-shift.r
+++ budget-2008-shift.r
@@ -5,12 +5,15 @@ library(scales)
 
 tax <- read.csv(
   'grandma/tax-rates.csv',
+  strip.white = T,
   colClasses = c('numeric', 'factor', 'factor', 'numeric')
 )
 tax$rate <- tax$rate/1000
 
-rates.over.time <- ggplot(na.omit(tax)) +
-  aes(x = year, y = rate, group = paste(bill, tax), color = paste(bill, tax)) +
+# rates.over.time <- ggplot(na.omit(tax)) +
+rates.over.time <- ggplot(tax) +
+  aes(x = year, y = rate, group = paste(bill, tax), color = paste(bill, 'bill,', tax, 'tax')) +
   scale_x_continuous('Year', breaks=unique(tax$year)) +
   scale_y_continuous('Tax rate (percent of assessed value)', labels = percent) +
+  labs(title = 'Scarsdale tax rates over time') +
   geom_line()
