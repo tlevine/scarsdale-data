0f53f261dd8bb846d4c245b0ceba3e9b4a47e4e5
Thomas Levine <occurrence@thomaslevine.com>
2012-12-10 19:45:21 -0500

    yay plot

--- 04-adopted_v_estimated.r
+++ 04-adopted_v_estimated.r
@@ -1,1 +1,10 @@
-cp <- read.csv('capital_projects-adopted_estimated.csv', stringsAsFactors = F)
+library(ggplot2)
+
+cp <- read.csv('budget/capital_projects-adopted_estimated-sum.csv',
+  sep = ',', strip.white = T
+)[-4]
+
+# Combine adopted and requested
+levels(cp$figure)[3] <- levels(cp$figure)[1]
+
+p <- ggplot(cp) + aes(x = year, y = amount_sum, group = paste(capital_project, figure), linetype = figure, color = capital_project) + geom_line()
