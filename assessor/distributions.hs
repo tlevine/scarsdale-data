dd5e0a0a9e5bd415335a76608c641158a75e71a2
Thomas Levine <occurrence@thomaslevine.com>
2012-12-24 19:43:52 -0500

    import

--- assessor/distributions.hs
+++ assessor/distributions.hs
@@ -1,4 +1,6 @@
 -- Retrieve only the taxable values for each house; retrieving identifying
 -- fields would be more work, so don't worry about them for now.
 
+import Text.Regex.Posix
+
 taxable="(COUNTY|VILLAGE|SCHOOL) TAXABLE ([0-9,]+)"
