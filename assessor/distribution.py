f94d9136501870ec37a4beb321a285ee8ecf30b7
Thomas Levine <occurrence@thomaslevine.com>
2012-12-24 21:43:32 -0500

    python

---
+++ assessor/distribution.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+import sys
+import re
+
+SNIP = re.compile(r"(COUNTY|VILLAGE|SCHOOL) TAXABLE ([0-9,]+)")
+snips = re.findall(SNIP, sys.stdin.read().replace('\n', ' '))
+print 'TAX,ASSESSED'
+for tax, assessed in snips:
+    print('%s,%d' % (tax, int(assessed.replace(',', ''))))
