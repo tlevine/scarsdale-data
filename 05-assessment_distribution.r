4d42b26fcbec6b06a512d05c138843b40ad1d20a
Thomas Levine <occurrence@thomaslevine.com>
2012-12-24 22:16:52 -0500

    aoeu

--- 05-assessment_distribution.r
+++ 05-assessment_distribution.r
@@ -12,11 +12,22 @@ if (sum(assess$TAX != c('COUNTY', 'VILLAGE', 'SCHOOL')) > 0) {
 # Combine by row.
 assess$PARCEL <- rep(1:(nrow(assess)/3), each = 3)
 
-
-p <- aaply(levels(assess$TAX), 1, function(tax){
+p.distributions <- aaply(levels(assess$TAX), 1, function(tax){
   ggplot(subset(assess, TAX == tax)) + aes(x = ASSESSED) +
     scale_x_log10('Assessed value', labels = dollar) +
     scale_y_continuous('Count') +
     labs(title = paste('Distribution of assessed values for', tax, 'taxes')) +
     geom_histogram(position = 'dodge')
 })
+
+assess.cast <- ddply(assess, 'PARCEL', function(df){data.frame(
+  mean = mean(df$ASSESSED),
+  sd = sd(df$ASSESSED),
+  label = paste(df$ASSESSED, collapse = '; ')
+)})
+mu <- mean(assess.cast$mean)
+p.variation <- ggplot(subset(assess.cast, sd > 0)) +
+  scale_x_continuous('Mean tax amount', labels = dollar) +
+  scale_y_continuous('Standard deviation of tax amount', labels = dollar) +
+  aes(x = mean, y = sd, label = label) +
+  geom_text()
