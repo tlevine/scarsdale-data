4f4f55cd8a78ae4eb23ec9132f4f52279c293eb7
Thomas Levine <occurrence@thomaslevine.com>
2012-12-24 21:20:11 -0500

    python csv

--- assessor/distributions.py
+++ assessor/distributions.py
@@ -4,5 +4,6 @@ import re
 
 SNIP = re.compile(r"(COUNTY|VILLAGE|SCHOOL) TAXABLE ([0-9,]+)")
 snips = re.findall(SNIP, sys.stdin.read().replace('\n', ' '))
+print 'TAX,ASSESSED'
 for tax, assessed in snips:
-    print('%s,%d' % tax, int(assessed.replace(',', '')))
+    print('%s,%d' % (tax, int(assessed.replace(',', ''))))
