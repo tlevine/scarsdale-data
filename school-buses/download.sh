d227f596a3cc8465442334453829b0b291be8370
Thomas Levine <occurrence@thomaslevine.com>
2013-12-18 20:01:14 +0000

    bus schedules

---
+++ school-buses/download.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -O 2012.pdf http://www.scarsdaleschools.org/cms/lib5/NY01001205/Centricity/Domain/13/SHSBusSchedule.pdf
+wget -O 2013.pdf http://www.scarsdaleschools.k12.ny.us/cms/lib5/NY01001205/Centricity/Domain/13/SHS%20Bus%20Schedule%202013.pdf
